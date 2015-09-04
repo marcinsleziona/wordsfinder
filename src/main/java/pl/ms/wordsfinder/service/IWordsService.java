061fdf79bf3e4751ae31860e3ae55ff8d20d9cfd
Marcin Sleziona <marcin.sleziona@gmail.com>
2015-09-04 15:38:27 +0200

    fix comment

--- src/main/java/pl/ms/wordsfinder/service/IWordsService.java
+++ src/main/java/pl/ms/wordsfinder/service/IWordsService.java
@@ -18,12 +18,12 @@ public interface IWordsService {
      * pattern -> te?t, gives: i.e. test, tent, ...
      *
      * @param pattern - can contain '?'
-     * @return - list of wordsfinder in alphabetical order
+     * @return - list of words in alphabetical order
      */
     List<String> findMatchingWords(String pattern);
 
     /**
-     * finds all possible wordsfinder that contain all the provide letters.
+     * finds all possible words that contain all the provide letters.
      * letters -> a?k, returns: rak, kra, ...
      *
      * @param letters - can contain '?'
@@ -32,7 +32,7 @@ public interface IWordsService {
     List<String> findPossibleWords(String letters);
 
     /**
-     * find all possible wordsfinder that can be build with the letters from 2-digits up to amount of letters
+     * find all possible words that can be build with the letters from 2-digits up to amount of letters
      * letters -> a?k, returns: aa, as, ka, kra, rak, ...
      *
      * @param letters - can contain '?'
